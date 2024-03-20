6d46c5348bdf911cd2887e777189605be8ddffdd
Bahaeeddine27 <kiliminelbooking@gmail.com>
2024-03-20 16:28:05 +0100

    middlewares

--- app/routes/wood.js
+++ app/routes/wood.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const woodCtrl = require("../controllers/wood.js");
+const auth = require("../middleware/auth.js")
 const router = express();
 
-router.get("/", woodCtrl.readAll);
-router.get("/hardness/:hardness", woodCtrl.readByHardness);
+router.get("/", auth, woodCtrl.readAll);
+router.get("/hardness/:hardness", auth, woodCtrl.readByHardness);
 
 module.exports = router;
