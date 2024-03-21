044a2175ecb3a363cc96c7b14b68f6fe4f4f5298
Bahaeeddine27 <kiliminelbooking@gmail.com>
2024-03-21 11:10:11 +0100

    ajout image

--- app/routes/wood.js
+++ app/routes/wood.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const woodCtrl = require("../controllers/wood.js");
-const auth = require("../middleware/auth.js")
+const auth = require("../middlewares/auth.js");
+const multer = require('../middlewares/multer.js');
+
 const router = express();
 
 router.get("/", auth, woodCtrl.readAll);
 router.get("/hardness/:hardness", auth, woodCtrl.readByHardness);
+router.post("/", auth, multer, woodCtrl.create);
 
 module.exports = router;
